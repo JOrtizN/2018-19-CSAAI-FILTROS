3d3ef2bb7ccc0f34ef50c3896e856c1c75c579b3
Julia Ortiz Naharro <jortizna@beta03.aulas.gsyc.urjc.es>
2019-04-30 10:17:19 +0200

    ya hace todo

--- filtro0.js
+++ filtro0.js
@@ -11,13 +11,11 @@ function main() {
   deslizador = document.getElementById('deslizador')
   deslizadorV = document.getElementById('deslizador2')
   deslizadorA = document.getElementById('deslizador3')
-  deslizadorG = document.getElementById('deslizador4')
-
+  bgris = document.getElementById('button')
   //-- Valor del deslizador
   range_value = document.getElementById('ver')
   rv_verde = document.getElementById('ver2')
   rv_azul = document.getElementById('ver3')
-  rv_gris = document.getElementById('ver4')
 
   //-- Se establece como tamaño del canvas el mismo
   //-- que el de la imagen original
@@ -32,6 +30,7 @@ function main() {
   ctx.drawImage(img, 0,0);
 
   function color() {
+    ctx.drawImage(img, 0,0);
     var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
     //-- Obtener el array con todos los píxeles
@@ -55,6 +54,10 @@ function main() {
     //-- Poner la imagen modificada en el canvas
     ctx.putImageData(imgData, 0, 0);
   }
+  deslizador.oninput = () => {
+    range_value.innerHTML = deslizador.value
+    color()
+    }
 
   deslizadorV.oninput = () => {
     rv_verde.innerHTML = deslizadorV.value
@@ -64,8 +67,21 @@ function main() {
     rv_azul.innerHTML = deslizadorA.value
     color()
     }
-  deslizador.oninput = () => {
-    range_value.innerHTML = deslizador.value
-    color()
+  bgris.onclick = () => {
+    ctx.drawImage(img, 0,0);
+    console.log('gris');
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    var data = imgData.data
+
+    for (var i = 0; i < data.length; i+=4){
+      var r = data[i];
+      var g = data[i+1];
+      var b = data[i+2];
+      brillo = (3 * r + 4 * g + b)/8;
+      data[i] = data[i+1] = data[i+2] = brillo;
     }
+    ctx.putImageData(imgData, 0, 0);
   }
+}
